89e1d7ed63a4d06cc86c4c7669e948dddef79c26
mikakane <mikakane2@gmail.com>
2016-07-19 18:58:15 +0900

    update

--- public/script.js
+++ public/script.js
@@ -1,44 +1,23 @@
 
 var loader = function(query){
-
-    var query = query || {};
-
-    //var url = "./api.json";
-    var url = "./api/event.php";
-
-    var xhr = $.get(url,query);
-    xhr.success(function(data){
-        var $target = $("#eventList");
-        $target.html("");
-        data.events.forEach(function(item){
-            var html = "<li><a href='"+item.event_url+"'>"+item.title+"</a></li>"
-            $target.append($(html))
-        })
-    })
-
-    xhr.error(function(){
-        alert("読み込みに失敗しました。")
-    })
     return xhr;
 }
 
 $(function(){
-    loader();
+    var app = new Vue({
+        el: "#app",
+        data: {
+            keyword: "leccafe",
+            events: []
+        },
+        created: function(){
+            var url = "./api/event.php";
+            var xhr = $.get(url,{
+                keyword: "leccafe"
+            });
+            xhr.success(function(response){
+                app.events = response.events
+            })
+        }
+    })
 });
-
-
-
-$(function(){
-    $(".pagination a").on("click",function(){
-        var start = $(this).data("start")
-        loader({
-            start:start * $("#numberCount").val(),
-            count: $("#numberCount").val(),
-            keyword: $("[name=keyword]:checked").val()
-        })
-    })
-
-    $("#numberCount").on("focusout",function(){
-        console.log("hgohoe")
-    })
-})
